27aabb44e4f65967dbe89ffe926afe49d9164988
Dani <danisweetwood@gmail.com>
2024-01-29 17:36:20 -0600

    added thought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,5 +8,53 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err);
         }
-    }
+    },
+
+    async createThought(req, res) {
+        try {
+          const thought = await Thought.create(req.body);
+          const user = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: thought._id } },
+            { new: true }
+          );
+    
+          if (!user) {
+            return res.status(404).json({
+              message: 'Thought created, but found no user with that ID',
+            });
+          }
+    
+          res.json('Created new thought 🎉');
+        } catch (err) {
+          console.log(err);
+          res.status(500).json(err);
+        }
+      },
+
+      async deleteThought(req, res) {
+        try {
+          const thought = await Thought.findOneAndRemove({ _id: req.params.thoughtId });
+    
+          if (!thought) {
+            return res.status(404).json({ message: 'No thought with this id!' });
+          }
+    
+          const user = await User.findOneAndUpdate(
+            { thoughts: req.params.thoughtId },
+            { $pull: { thoughts: req.params.thoughtId } },
+            { new: true }
+          );
+    
+          if (!user) {
+            return res
+              .status(404)
+              .json({ message: 'Thought deleted but no user with this id!' });
+          }
+    
+          res.json({ message: 'Thought successfully deleted!' });
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      }
 }
